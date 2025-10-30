01b81b3923af5c87aacb62948188e994c3b8075f
dla1934-byte <dla1934@sookmyung.ac.kr>
2025-10-30 16:14:25 +0900

    Update main.c

--- main.c
+++ main.c
@@ -3,20 +3,22 @@
 #define SIZE 5
 int main(int argc, char *argv[])
 {
-  int i;
-  int grade[SIZE];
-  int score[SIZE];
-  
-  for(i=0;i<SIZE;i++)
-  {
-                     grade[i]=rand()%100;
-                     score[i]=grade[i];
-  }
-  #if 1
-  for(i=0;i<SIZE; i++)
-     printf("grade[%i]=%i, score[%i]=%i\n", i, grade[i], i, score[i]);
-  #endif
-  
+    int i;
+    int a[SIZE]={1,2,3,4,5};
+    int b[SIZE]={1,2,3,4,5};
+    int flag_same=1;
+    
+    for(i=0;i<SIZE;i++)
+    {
+                       if(a[i] !=b[i])
+                       {
+                               printf("%i-th element is diff.\n", i);
+                               flag_same=0;
+                       }
+     }
+     printf("Are a and b the same? %i\n", flag_same);
+     
+ 
 
 
 
